2c27cc7d3be79cef0339eeab7fb8ea884894cc36
Tarek Quao <tarek.quao@gmail.com>
2025-10-24 14:16:29 +0000

    Included createdAt & updatedAt fields for better auditing of user entity

--- src/main/java/com/dexwin/notesapp/entity/User.java
+++ src/main/java/com/dexwin/notesapp/entity/User.java
@@ -3,6 +3,10 @@ package com.dexwin.notesapp.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "users")
@@ -21,4 +25,12 @@ public class User {
 
     @Column(nullable = false)
     private String role = "USER";
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
 }
